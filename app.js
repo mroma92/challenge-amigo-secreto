9f0c0218014e383a9b0f32511ead6c743edc1bb5
Martin <romagnoli92@gmail.com>
2025-03-14 22:24:10 +0100

    Primer avance

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos=[];
+
+
+function agregarAmigo(){
+    const nuevoAmigo = document.getElementById('amigo').value;
+    console.log(nuevoAmigo)
+    if (nuevoAmigo ===''){
+        alert("Por favor, inserte un nombre.");
+        
+    } else{  
+        
+        amigos.push(nuevoAmigo);
+        document.querySelector('#amigo').value ='';
+    }
+    console.log(amigos);
+    
+    return;
+}
+
+
+
+function listaSorteo(){
+    let  = document.getElementById("myP").innerHTML;
+    for (let i = 0; i < amigos.length; i++) {
+    text += amigos[i] + "<br>";
+
+    document.getElementById("demo").innerHTML = text;
+    }
+
+}
+
+function sortearAmigo(){
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+}
+
+// nuevoAmigo.innerHTML = ;
